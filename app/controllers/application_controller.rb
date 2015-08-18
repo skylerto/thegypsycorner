065432bdcbff3c3d0e24ca0023f98752653cbd99
skylerto <skylerclayne@gmail.com>
2015-08-18 09:02:13 -0400

    added avatars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_devise_permitted_parameters
-    registration_params = [:name, :email, :password, :password_confirmation]
+    registration_params = [:name, :email, :password, :password_confirmation,  :avatar]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) {
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818123404) do
+ActiveRecord::Schema.define(version: 20150818124054) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 20150818123404) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
