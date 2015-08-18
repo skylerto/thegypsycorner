08b89c1fa14673b370b3ff15f7dd3cbc7daa035d
skylerto <skylerclayne@gmail.com>
2015-08-18 08:37:00 -0400

    added admin user to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817184515) do
+ActiveRecord::Schema.define(version: 20150818123404) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150817184515) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
